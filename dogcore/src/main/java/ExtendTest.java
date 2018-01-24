75602cdea6d23bb8caacde8a139a84bdb786e365
gadfly <pettygadfly@gmail.com>
2018-01-24 23:01:39 +0800

    init

---
+++ dogcore/src/main/java/ExtendTest.java
@@ -0,0 +1,9 @@
+/**
+ * author: 牛虻.
+ * time:2018/1/24 0024
+ * email:pettygadfly@gmail.com
+ * doc:
+ */
+public class ExtendTest {
+
+}
---
+++ dogwebroot/src/main/java/ExtendSubTest.java
@@ -0,0 +1,10 @@
+/**
+ * author: 牛虻.
+ * time:2018/1/24 0024
+ * email:pettygadfly@gmail.com
+ * doc:
+ * 这里可以通过依赖包，完成集成等基础框架。
+ * 可以在测试多模块，这个项目作为基础框架存在
+ */
+public class ExtendSubTest extends  ExtendTest {
+}
