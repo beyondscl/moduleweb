b5449786fca0947f67d647c581e0048abdad1e04
beyondscl <727030664@qq.com>
2018-02-01 13:51:19 +0800

    集成mybatis Generator不过貌似没什么用

--- dogdb/com/bird/dao/CountrylanguageDao.java
+++
@@ -1,5 +0,0 @@
-package com.bird.dao; 
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CountrylanguageDao extends BaseDao {
-}
--- dogdb/com/bird/dao/UserDao.java
+++
@@ -1,5 +0,0 @@
-package com.bird.dao; 
-import org.springframework.stereotype.Repository;
-@Repository
-public interface UserDao extends BaseDao {
-}
