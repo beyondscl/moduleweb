1a65c3e591e7e347dd0cf37393287ca1a718f5ed
gadfly <pettygadfly@gmail.com>
2018-02-05 21:02:12 +0800

    修改bean字段的类型为包装类型

--- dogueditor/src/main/java/com/bird/domain/ArticleDir.java
+++ dogueditor/src/main/java/com/bird/domain/ArticleDir.java
@@ -3,17 +3,32 @@ package com.bird.domain;
 import lombok.Data;
 
 @Data
+
 public class ArticleDir {
+
     //注释:  默认值: null 是否可为空 NO
+
     private String id;
+
     //注释: 作者id 默认值: null 是否可为空 YES
+
     private String authorId;
+
     //注释: 目录名称 默认值: null 是否可为空 YES
+
     private String dirName;
+
     //注释:  默认值: null 是否可为空 NO
+
     private String createTime;
-    //注释: 最后修改时间 默认值: null 是否可为空 NO
+
+    //注释: 最后修改时间 默认值: null 是否可为空 YES
+
     private String updateTime;
+
     //注释: 是否有效1有效;0无效 默认值: null 是否可为空 NO
-    private int effective;
+
+    private Integer effective;
+
 }
+ 
