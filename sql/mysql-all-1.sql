c2ed35740efe2973b06a0ffc5a75401a31bfc2e3
beyondscl <727030664@qq.com>
2018-02-07 11:08:06 +0800

    修改了sql

--- sql/mysql-all-1.sql
+++ sql/mysql-all-1.sql
@@ -1,28 +1,34 @@
-use world;
-show  global  variables like  'wait_timeout';  
-select * from user;
-update  user   set password = '123123' where id = '0';
+USE world;
+SHOW GLOBAL VARIABLES LIKE 'wait_timeout';
+SELECT *
+FROM user;
+UPDATE user
+SET password = '123123'
+WHERE id = '0';
 -- delete from user;
 
 TRUNCATE `world`.`user`;
 
-select * from user;
-
+SELECT *
+FROM user;
 
 ###查询表信息
-desc article;
-show full fields from article;
+DESC article;
+SHOW FULL FIELDS FROM article;
 
 
-use world;
-select * from all_tables WHERE owner='root';
+USE world;
+SELECT *
+FROM all_tables
+WHERE owner = 'root';
 
 ###查询所有表
-select table_name 
-from information_schema.tables 
-where table_schema='world';
+SELECT table_name
+FROM information_schema.tables
+WHERE table_schema = 'world';
 
-select * from article_dir;
+SELECT *
+FROM article_dir;
 
 
 
